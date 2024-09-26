9f227f1fc358d3dfa2e0fbd2e47c07ceeede09cd
weu-souza <weuller1561@gmail.com>
2024-09-26 17:41:54 -0300

    fix: fixing the video component

--- src/components/Player/VideoPlayer.tsx
+++ src/components/Player/VideoPlayer.tsx
@@ -1,50 +1,23 @@
-import { useEffect, useState } from "react";
-import { searchVideosByTitle, VideoItem } from "../../shared/API/Youtube/config";
 import { useLocation } from "react-router-dom";
 import PlayerHeader from "./PlayerHeader";
-import Playercontrols from "./PlayerControls";
-
-
-
-// Custom hook to get search params from the URL
 function useQuery() {
   return new URLSearchParams(useLocation().search);
 }
 
 function VideoPlayer() {
-  const [backgroundVideo, setBackgroundVideo] = useState<VideoItem | null>(
-    null
-  );
   const searchParams = useQuery();
   const title = searchParams.get("title") ?? "";
-
-  useEffect(() => {
-    async function fetchBackgroundVideo() {
-      const fetchedVideos = await searchVideosByTitle(title);
-      if (fetchedVideos.length > 0) setBackgroundVideo(fetchedVideos[0]);
-    }
-    fetchBackgroundVideo();
-  }, [title]);
-
   return (
-    <div className="video-container overflow-hidden h-screen">
-      <div className="rotate-mobile">
-        {backgroundVideo && (
-          <div className="iframe-container md:w-screen ">
+    <div className="flex overflow-hidden w-screen h-screen">
             <iframe
-              src={`https://www.youtube.com/embed/${backgroundVideo.id}?autoplay=0&mute=0&loop=1&controls=0`}
+              src="/public/loki_trailer.mp4" 
+              
               allow="autoplay; encrypted-media"
               allowFullScreen
-              className="h-full md:h-screen "
-            ></iframe>
-          </div>
-        )}
+              className="h-screen w-full"></iframe>
         <div className="z-10">
           <PlayerHeader titleMovie={title} />
-          <Playercontrols />
-        </div>
-      </div>
-    </div>
+        </div>      </div>
   );
 }
 
